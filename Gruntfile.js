6636be25ab95514f51d0a40a3d9e673f74c6ebd8
David Clarke <davidpclarke@gmail.com>
2014-08-10 12:06:27 +0100

    Added and configured Sass plugin for Grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,10 +4,21 @@ module.exports = function(grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
 
+        sass: {                                             // Task
+            dist: {                                         // Target
+                options: {                                  // Target options
+                    style: 'expanded'
+                },
+                files: {                                    // Dictionary of files
+                    'css/main.css': 'css/scss/main.scss'    // 'destination': 'source'
+                }
+            }
+        },
+
         concat: {   
             dist: {
                 src: [
-                    'js/vendor/modernizr-2.6.2.min.js',  // Modernizr (from H5BP)
+                    'js/vendor/modernizr-2.6.2.min.js', // Modernizr (from H5BP)
                     'js/*.js',                          // All JS in the js folder (from H5BP)
                     'js/vendor/jquery.fittext.js'       // FitText.js
                 ],
@@ -24,9 +35,10 @@ module.exports = function(grunt) {
     });
 
     // Where we tell Grunt we plan to use this plug-in.
+    grunt.loadNpmTasks('grunt-contrib-sass');
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-uglify');
 
     // Where we tell Grunt what to do when we type "grunt" into the terminal.
-    grunt.registerTask('default', ['concat', 'uglify']);
+    grunt.registerTask('default', ['sass', 'concat', 'uglify']);
 };
