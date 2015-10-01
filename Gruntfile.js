6231385c7c45111c84cec6a5e5e381bf693cd8e2
David Clarke <davidpclarke@gmail.com>
2015-10-01 18:13:15 +0100

    Enable grunt watch and live reload

--- Gruntfile.js
+++ Gruntfile.js
@@ -27,6 +27,17 @@ module.exports = function(grunt) {
                 src: 'js/build/production.js',
                 dest: 'js/build/production.min.js'
             }
+        },
+
+        // Watch Configuration
+        watch: {
+            files: "css/scss/*",
+            tasks: [ "sass" ],
+
+            options: {
+                livereload: true
+            }
+
         }
     });
 
@@ -34,6 +45,7 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-sass');
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks( "grunt-contrib-watch" );
 
     // Where we tell Grunt what to do when we type "grunt" into the terminal.
     grunt.registerTask('default', ['sass', 'concat', 'uglify']);
